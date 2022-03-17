42775d1296614b51ed2e3f8c93d13ca1d03b2d22
damendozam <47694888+damendozam@users.noreply.github.com>
2022-03-17 17:42:03 -0500

    Morfologia

--- index.py
+++ index.py
@@ -30,5 +30,19 @@ imshow('Otsu',th3)
 #Letras
 #print(pytesseract.image_to_string(th3))
 
+kernel = np.ones((5,5), np.uint8)
+ 
+# The first parameter is the original image,
+# kernel is the matrix with which image is
+# convolved and third parameter is the number
+# of iterations, which will determine how much
+# you want to erode/dilate a given image.
+img_erosion = cv2.erode(th3, kernel, iterations=1)
+img_dilation = cv2.dilate(th3, kernel, iterations=1)
+ 
+cv2.imshow('Input', img)
+cv2.imshow('Erosion', img_erosion)
+cv2.imshow('Dilation', img_dilation)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
