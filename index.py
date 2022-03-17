03e79b30e76758419fd9f1cbddc94389c8832834
damendozam <47694888+damendozam@users.noreply.github.com>
2022-03-17 18:04:13 -0500

    Actualización morfología

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 #Librería
 import cv2
 from cv2 import imshow
+import numpy as np
 """import pytesseract
 tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'"""
 
-imgText="cafe"
+imgText="people"
 #Lectura de imagen
 img= cv2.imread(imgText+'.png')
 imshow('Imagen Original',img)
@@ -12,7 +13,7 @@ imshow('Imagen Original',img)
 #Imagen escala de grises
 imgGrau= cv2.imread(imgText+'.png',0)
 imshow('Grises',imgGrau)
-
+"""
 #Binarización
 ret,tresh=cv2.threshold(imgGrau,150,255,cv2.THRESH_BINARY)
 imshow('Binarizada',tresh)
@@ -22,9 +23,10 @@ th1=cv2.adaptiveThreshold(imgGrau,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINA
 th2=cv2.adaptiveThreshold(imgGrau,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,40)
 imshow('Promedio',th1)
 imshow('Gaussiano',th2)
+"""
 
 #Binarización Otsu
-ret,th3=cv2.threshold(imgGrau,150,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+ret,th3=cv2.threshold(imgGrau,150,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 imshow('Otsu',th3)
 
 #Letras
@@ -37,10 +39,9 @@ kernel = np.ones((5,5), np.uint8)
 # convolved and third parameter is the number
 # of iterations, which will determine how much
 # you want to erode/dilate a given image.
-img_erosion = cv2.erode(th3, kernel, iterations=1)
-img_dilation = cv2.dilate(th3, kernel, iterations=1)
+img_erosion = cv2.erode(th3, kernel, iterations=5)
+img_dilation = cv2.dilate(th3, kernel, iterations=5)
  
-cv2.imshow('Input', img)
 cv2.imshow('Erosion', img_erosion)
 cv2.imshow('Dilation', img_dilation)
 
